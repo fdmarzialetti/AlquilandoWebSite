6028b222fd34cec148ac3484aca0ff8a703dd398
fdmarzialetti <fdmarzialetti@gmail.com>
2025-04-02 14:18:55 -0300

    first commit

---
+++ src/main/java/com/devgol53/rent_website/AlquilandoWebSiteApplication.java
@@ -0,0 +1,14 @@
+package com.devgol53.rent_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilandoWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilandoWebSiteApplication.class, args);
+		System.out.println("Hola");
+	}
+
+}
---
+++ src/test/java/com/devgol53/rent_website/AlquilandoWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgol53.rent_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilandoWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
