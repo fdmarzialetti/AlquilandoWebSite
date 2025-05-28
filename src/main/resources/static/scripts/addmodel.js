4d2fae8eda781324ea27c305f15e310f2004b237
MARTIN ESQUERCIA <martincitoesquercia@hotmail.com>
2025-05-28 16:40:30 -0300

    Agrego base para la carga de modelos, junto con formulario correspondiente

---
+++ src/main/resources/static/scripts/addmodel.js
@@ -0,0 +1,68 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      marca: "",
+      modelo: "",
+      precio: null,
+      capacidad: null,
+      politica: null,
+      imagen: null
+    };
+  }, created() { },
+  mounted() {
+
+  },
+  methods: {
+    validarEntero(event) {
+      const valor = event.target.value;
+      if (!/^\d*$/.test(valor)) {
+        event.target.value = this.capacidad || '';
+      } else {
+        this.capacidad = parseInt(valor);
+      }
+    },
+    handleFileUpload(event) {
+      this.imagen = "imegen autito";
+    },
+    async createModel() {
+      try {
+        let modelo = {
+          brand: this.marca,
+          name: this.modelo,
+          price: this.precio,
+          image: this.imagen,
+          capacity: this.capacidad,
+          cancelationPolicy: this.politica
+        }
+        const res = await axios.post('/api/model/create', modelo).then(respuesta => {
+          console.log("Modelo creado:", respuesta);
+          Swal.fire({
+            title: "Modelo creado con exito!",
+            icon: "success",
+            draggable: true
+          }).then(respuesta => {
+
+            window.location.href = "./admin.html";
+          });
+
+
+        })
+
+
+
+
+
+
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: "Error en la carga, debido a que ya existe este modelo en el sistema",
+          text: "Intenta con otros datos",
+        });
+        console.error(error);
+      }
+    }
+  }
+}).mount('#app');
