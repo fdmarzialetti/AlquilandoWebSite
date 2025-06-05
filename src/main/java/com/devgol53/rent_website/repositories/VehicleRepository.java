c5d9e2198e590a7c183771333253c0d0d6fd3e24
emilianoross11 <emilianoross649@gmail.com>
2025-06-05 12:57:30 -0300

    addvehicle

--- src/main/java/com/devgol53/rent_website/repositories/VehicleRepository.java
+++ src/main/java/com/devgol53/rent_website/repositories/VehicleRepository.java
@@ -1,10 +1,10 @@
 package com.devgol53.rent_website.repositories;
 
-
 import com.devgol53.rent_website.entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    boolean existsByPatent(String patent);
 }
