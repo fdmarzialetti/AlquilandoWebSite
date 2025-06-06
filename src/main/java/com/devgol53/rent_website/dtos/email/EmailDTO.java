24f4cb893674f1585b6c84ea60dc54cdfa923fbc
Fernando Marzialetti <fdmarzialetti@gmail.com>
2025-06-06 15:33:39 -0300

    MailSender y envio de email cuando se realiza una reserva

---
+++ src/main/java/com/devgol53/rent_website/dtos/email/EmailDTO.java
@@ -0,0 +1,25 @@
+package com.devgol53.rent_website.dtos.email;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailDTO {
+    private String destinatario;
+    private String asunto;
+    private String mensaje;
+    private String branch;
+    private String pickupDate;
+    private String vehicle;
+
+    public EmailDTO(String destinatario, String asunto, String mensaje, String branch, String pickupDate, String vehicle) {
+        this.destinatario = destinatario;
+        this.asunto = asunto;
+        this.mensaje = mensaje;
+        this.branch = branch;
+        this.pickupDate = pickupDate;
+        this.vehicle = vehicle;
+    }
+}
+
