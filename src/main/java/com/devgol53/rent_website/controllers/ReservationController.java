49b459416160352f7410ffd38653565331a0d68e
Fernando Marzialetti <fdmarzialetti@gmail.com>
2025-06-06 10:53:39 -0300

    validaciones de pago

--- src/main/java/com/devgol53/rent_website/controllers/ReservationController.java
+++ src/main/java/com/devgol53/rent_website/controllers/ReservationController.java
@@ -1,14 +1,8 @@
 package com.devgol53.rent_website.controllers;
 
 import com.devgol53.rent_website.dtos.reservation.ReservationPostDto;
-import com.devgol53.rent_website.entities.AppUser;
-import com.devgol53.rent_website.entities.Branch;
-import com.devgol53.rent_website.entities.Model;
-import com.devgol53.rent_website.entities.Reservation;
-import com.devgol53.rent_website.repositories.AppUserRepository;
-import com.devgol53.rent_website.repositories.BranchRepository;
-import com.devgol53.rent_website.repositories.ModelRepository;
-import com.devgol53.rent_website.repositories.ReservationRepository;
+import com.devgol53.rent_website.entities.*;
+import com.devgol53.rent_website.repositories.*;
 import com.devgol53.rent_website.utils.CodeGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,6 +16,8 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/reservation")
 public class ReservationController {
+    @Autowired
+    private CardRepository cardRepository;
     @Autowired
     private ReservationRepository reservationRepository;
     @Autowired
@@ -39,24 +35,40 @@ public class ReservationController {
 
     @PostMapping("/createReservation")
     public ResponseEntity<String> createReservation(@RequestBody ReservationPostDto reservationPostDto, Authentication auth) {
-        String newCode;
-        do {
-            newCode = CodeGenerator.generarCodigoAlfanumerico();
-        } while (reservationRepository.findByCode(newCode).isPresent());
 
-        Branch branchFind = branchRepository.findById(reservationPostDto.getBranch())
-                .orElseThrow(() -> new RuntimeException("Branch not found"));
-        Model modelFind = modelRepository.findById(reservationPostDto.getModel())
-                .orElseThrow(() -> new RuntimeException("Model not found"));
-        AppUser client = appUserRepository.findByEmail(auth.getName())
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        Optional<Card> findCard = cardRepository.findByNumber(reservationPostDto.getCard().getNumber());
+        if (!findCard.isPresent()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La tarjeta ingresada no es válida. Por favor, verifique los datos e intente nuevamente.");
+        }
+        if(findCard.isPresent()){
+            if(findCard.get().getNumber().equals("4567456745674567")){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Saldo insuficiente. Por favor, revise los fondos disponibles en su cuenta.");
+            }
+        }
 
-        Reservation newReservation = new Reservation(newCode, reservationPostDto);
-        newReservation.addBranch(branchFind);
-        newReservation.addModel(modelFind);
-        newReservation.addClient(client);
-        reservationRepository.save(newReservation);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Reserva realizada con exito");
+        if(findCard.isPresent()){
+            if(findCard.get().getNumber().equals("1234123412341234")){
+                String newCode;
+                do {
+                    newCode = CodeGenerator.generarCodigoAlfanumerico();
+                } while (reservationRepository.findByCode(newCode).isPresent());
+
+                Branch branchFind = branchRepository.findById(reservationPostDto.getBranch())
+                        .orElseThrow(() -> new RuntimeException("Branch not found"));
+                Model modelFind = modelRepository.findById(reservationPostDto.getModel())
+                        .orElseThrow(() -> new RuntimeException("Model not found"));
+                AppUser client = appUserRepository.findByEmail(auth.getName())
+                        .orElseThrow(() -> new RuntimeException("User not found"));
+
+                Reservation newReservation = new Reservation(newCode, reservationPostDto);
+                newReservation.addBranch(branchFind);
+                newReservation.addModel(modelFind);
+                newReservation.addClient(client);
+                reservationRepository.save(newReservation);
+                return ResponseEntity.status(HttpStatus.CREATED).body("Reserva realizada con éxito! Recibirá el codigo de reserva en su correo electrónico.");
+            }
+        }
+        return null;
     }
 
 }
